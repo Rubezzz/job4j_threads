abc1b37ec570d8bc96f7291a20832056d289bba8
Rubezzz <inigor1@mail.ru>
2023-11-27 16:24:30 +0700

    3. Состояние нити. [#229175 #462606]

--- src/main/java/ru/job4j/concurrent/ThreadState.java
+++ src/main/java/ru/job4j/concurrent/ThreadState.java
@@ -10,8 +10,9 @@ public class ThreadState {
         );
         first.start();
         second.start();
-        while (first.getState() != Thread.State.TERMINATED && second.getState() != Thread.State.TERMINATED) {
-            continue;
+        while (first.getState() != Thread.State.TERMINATED || second.getState() != Thread.State.TERMINATED) {
+            System.out.printf("Fist thread: %s%s", first.getState(), System.lineSeparator());
+            System.out.printf("Second thread: %s%s", second.getState(), System.lineSeparator());
         }
         System.out.println("Программа завершена.");
     }
