c3feaa6538f34d18dbb5c9694d63d255a27620eb
Rubezzz <inigor1@mail.ru>
2023-12-07 15:29:23 +0700

    1. Неблокирующий кеш [#4741]

---
+++ src/main/java/ru/job4j/cache/Cache.java
@@ -0,0 +1,36 @@
+package ru.job4j.cache;
+
+import java.util.Map;
+import java.util.Objects;
+import java.util.Optional;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.function.BiFunction;
+import java.util.stream.Stream;
+
+public class Cache {
+    private final Map<Integer, Base> memory = new ConcurrentHashMap<>();
+
+    public boolean add(Base model) throws OptimisticException {
+        return memory.putIfAbsent(model.id(), model) == null;
+    }
+
+    public boolean update(Base model) throws OptimisticException {
+        BiFunction<Integer, Base, Base> func = (a, b) -> {
+            if (model.version() != b.version()) {
+                throw new OptimisticException("Versions are not equal");
+            }
+            return new Base(model.id(), model.name(), model.version() + 1);
+        };
+        return memory.computeIfPresent(model.id(), func) != null;
+    }
+
+    public void delete(int id) {
+        memory.remove(id);
+    }
+
+    public Optional<Base> findById(int id) {
+        return Stream.of(memory.get(id))
+                .filter(Objects::nonNull)
+                .findFirst();
+    }
+}
